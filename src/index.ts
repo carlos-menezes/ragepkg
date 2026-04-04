25c8575832dfce82e85bd20659aec437f3c4d7a5
Carlos Menezes <carlos.dm.menezes@gmail.com>
2026-04-04 12:27:10 +0100

    chore: fix Changesets commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { program } from "commander";
+import pkgJson from "../package.json" with { type: "json" };
 import { ensureCommand } from "./commands/ensure.js";
 
+program
+	.name("ragepkg")
+	.description(
+		"Multiplatform CLI tool to aid in downloading and managing RageMP server binaries",
+	)
+	.version(pkgJson.version);
 program.addCommand(ensureCommand);
 
 program.parse();
